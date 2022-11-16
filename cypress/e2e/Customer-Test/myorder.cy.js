5b052e5c63607acc82e9135ce69095ca0f78bcca
delight87 <oparabethrand@gmail.com>
2022-11-16 21:49:16 +0100

    requestpickup updated

--- cypress/e2e/Customer-Test/myorder.cy.js
+++ cypress/e2e/Customer-Test/myorder.cy.js
@@ -2,27 +2,42 @@ import {myorder} from "../../fixtures/customer-Selectors.js";
 const access  = require("./validLogin.cy.js");
 
 describe("myorder", function () {
-    before(function () {
+    beforeEach(function () {
         cy.visit('https://sharply-test.sharply.africa/auth/login')
         access.validLogin();
 
     });
-
-    it("MY ORDER - As a customer ", function () {
+    it("MY ORDER - Create Order ", function () {
         cy.get(myorder.myorder1).click()
+        
+    })
+    it("My Order- As a Customer", function () {
+        cy.get(myorder.myorder11).click()
         cy.get(myorder.searchBar).type(myorder.searchbarTxt)
-        cy.get(myorder.creatOrder).click()
-        cy.get(myorder.receiver).click()
-        cy.get(myorder.thirdParty).click()
-        cy.get(myorder.deliveryArea).click()
-        cy.get(myorder.chooseArea).click()
-        cy.wait(3000)
-        // cy.get(myorder.pickupDetails).click({force:true})
-        // cy.wait(3000)
-        // cy.get(myorder.senderName).type(myorder.sendernameTxt),({force:true})
-        cy.wait(3000)
-        // cy.get(myorder.dropoffDetails).click()
-        cy.wait(3000)
-        cy.get(myorder.packageDetails).type(myorder.detailTxt), ({force:true})
-    });
+        cy.get(myorder.checkexistingorder).click() 
+        // cy.get(myorder.backBtn1).click({force:true})
+        cy.get(myorder.createOderBtnn).click()
+        cy.get(myorder.asasender).click({force:true})
+        cy.get(myorder.asaReceiver).click()
+        cy.get(myorder.asaThirdparty).click()
+        cy.get(myorder.deliveryarea1).click()
+        cy.get(myorder.selectArea).click()
+        cy.get(myorder.pickupDetailss).click()
+        // cy.get(myorder.senderDetailss).type(requestpickup.senderText)
+        // cy.get(myorder.senderNumber1x).click()
+        // cy.get(myorder.senderNumbersx).select('Nigeria')
+        cy.get(myorder.dropoffDetails).click()
+        // cy.get(myorder.receipientNamee).type(myorder.receipientText)
+        // cy.get(myorder.selectreceiverCountry).click()
+        // cy.get(myorder.countrynameText).select('Nigeria')
+        // cy.get(myorder.receipientAddresss).type(myorder.receipientaddressText1)
+    // //  cy.get(myorder.packageDetailsx).type(myorder.packageText)
+    // //  cy.get(myorder.selectQuantity).select('2')
+            // cy.get(myorder.additemBtn).click()
+    // //     cy.get(myorder.vehicleType).click()
+            cy.get(myorder.allowDescription).click()
+            cy.get(myorder.descriptionbox).type(myorder.descriptionTxt)
+            cy.get(myorder.paymentonDelivery).click()
+        cy.get(myorder.submitRequest).click()
+})
 })
--- cypress/e2e/Customer-Test/requestpickup.cy.js
+++ cypress/e2e/Customer-Test/requestpickup.cy.js
@@ -1,32 +1,73 @@
-import {Request} from "../../fixtures/customer-Selectors.js";
+import {requestpickup} from "../../fixtures/customer-Selectors.js";
 const access  = require("./validLogin.cy.js");
 
-describe("login", function () {
-    beforeEach(function () {
+describe("requestpickup", function () {
+    before(function () {
         cy.visit('https://sharply-test.sharply.africa/auth/login')
         access.validLogin();
+
     });
 
+    it("Request pickup - As a receiver ", function () {
+        cy.get(requestpickup.requestPickup1).click()
+        
+    })
+
     it("Request pickup - As a sender ", function () {
-        cy.get().click()
+        cy.get(requestpickup.asasender).click()
         
     })
 
     it("Request pickup - As a receiver ", function () {
-        cy.get(Homepage.requestPickup).click()
-        
+        cy.get(requestpickup.asaReceiver).click()
+
     })
 
-    it("Request pickup - As a third party ", function () {
-        cy.get(Homepage.requestPickup).click()
-        
+    it("Request pickup - As a third Party ", function () {
+        cy.get(requestpickup.asaThirdparty).click()
+
     })
 
+    it("Request pickup - Delivery area ", function () {
+        cy.get(requestpickup.deliveryarea1).click()
 
+    })
+    it("Request pickup - Select Area ", function () {
+        cy.get(requestpickup.selectArea).click()
 
+    })
+    it("Request pickup - Pickup Details ", function () {
+        cy.get(requestpickup.pickupDetailss).click()
+        // cy.get(requestpickup.senderDetailss).type(requestpickup.senderText)
+        // cy.get(requestpickup.senderNumber1x).click()
+        // cy.get(requestpickup.senderNumbersx).select('Nigeria')
 
+    })
+    it("Request pickup - Drop off details ", function () {
+        cy.get(requestpickup.dropoffDetails).click()
+        // cy.get(requestpickup.receipientNamee).type(requestpickup.receipientText)
+        // cy.get(requestpickup.selectreceiverCountry).click()
+        // cy.get(requestpickup.countrynameText).select('Nigeria')
+        // cy.get(requestpickup.receipientAddresss).type(requestpickup.receipientaddressText1)
 
+    // })
+    // // it("Request pickup - Package Details ", function () {
+    // //     cy.get(requestpickup.packageDetailsx).type(requestpickup.packageText)
+    // //     cy.get(requestpickup.selectQuantity).select('2')
+    //         // cy.get(requestpickup.additemBtn).click()
 
+    // })
+    // // it("Request pickup - Vehicle type ", function () {
+    // //     cy.get(requestpickup.vehicleType).click()
 
+    })
+    it("Request pickup - Description checkbox ", function () {
+            cy.get(requestpickup.allowDescription).click()
+            cy.get(requestpickup.descriptionbox).type(requestpickup.descriptionTxt)
+            cy.get(requestpickup.paymentonDelivery).click()
 
+    })
+    it("Request pickup - Submit Request ", function () {
+        cy.get(requestpickup.submitRequest).click()
+})
 })
