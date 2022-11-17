e100c4c3e9966cb4b0b852083f0efef9a15282ad
delight87 <oparabethrand@gmail.com>
2022-11-17 11:27:18 +0100

    i just updated request pickup

--- cypress/e2e/Customer-Test/homepage.cy.js
+++ cypress/e2e/Customer-Test/homepage.cy.js
@@ -2,37 +2,47 @@ import {Homepage} from "../../fixtures/customer-Selectors.js";
 const access  = require("./validLogin.cy.js");
 
 describe("login", function () {
-    beforeEach(function () {
+    before(function () {
         cy.visit('https://sharply-test.sharply.africa/auth/login')
         access.validLogin();
-    });
-
-    it("HOMEPAGE- View homepage cards", function () {
+    })
+    it("click on request pickup",function(){
         cy.get(Homepage.requestPickup).click()
-        cy.wait(3000)
-        cy.get(Homepage.backBtn).click()
-        cy.get(Homepage.trackOrder).click()
-        cy.wait(3000)
-        cy.get(Homepage.backBtn).click()
-        cy.get(Homepage.myOrders).click()
-        cy.wait(3000)
-        cy.get(Homepage.backBtn).click()
-        cy.get(Homepage.myAccount).click()
-        cy.wait(3000)
+    })
+    it("click on home page button ",function(){
         cy.get(Homepage.backBtn).click()
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
+    it("clcik on track order button ",function(){
+        cy.get(Homepage.trackOrder).click()
+       
+    })
+    it("wait for 3 seconds ",function(){
+        //cy.wait(3000)
+    })
+    it("click on back button ",function(){
+        cy.get(Homepage.backBtn).click()
+    })
+    it("click on my orders ",function(){
+        cy.get(Homepage.myOrders).click()
+        
+    })
+    it("wait for 3 seconds ",function(){
+        cy.wait(3000)
+        
+    })
+    it("click on back button",function(){
+        cy.get(Homepage.backBtn).click()
+        
+    })
+    it("click on my accoount button",function(){
+        cy.get(Homepage.myAccount).click()
+    })
+    it("wait 3 seconds ",function(){
+        cy.wait(3000)
+        
+    })
+    it("click on back button ",function(){
+        cy.get(Homepage.backBtn).click()
+    })
 
 })
